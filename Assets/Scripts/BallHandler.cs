ffeef7b1c51fe24fe74b4870354ce99ccb3a9b8c
KlarkKe <aliakbergasanov@gmail.com>
2025-11-01 17:21:11 +0300

    upd

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,16 +4,20 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D currentBallRigidbody;
-    [SerializeField] private SpringJoint2D currentBallSprintJoint;
-    [SerializeField] private float delayDuration = 0.5f;
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
 
+    private Rigidbody2D currentBallRigidbody;
+    private SpringJoint2D currentBallSprintJoint;
     private Camera mainCamera;
     private bool isDragging;
 
     private void Start()
     {
         mainCamera = Camera.main;
+        SpawnNewBall();
     }
 
     void Update()
@@ -40,17 +44,29 @@ public class BallHandler : MonoBehaviour
         currentBallRigidbody.position = worldPosition;
     }
 
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSprintJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSprintJoint.connectedBody = pivot;
+    }
+
     private void LaunchBall()
     {
         currentBallRigidbody.bodyType = RigidbodyType2D.Dynamic;
         currentBallRigidbody = null;
 
-        Invoke("DetachBall", delayDuration);
+        Invoke("DetachBall", detachDelay);
     }
 
     private void DetachBall()
     {
         currentBallSprintJoint.enabled = false;
         currentBallSprintJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 }
